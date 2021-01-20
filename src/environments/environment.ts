83f58d7fe1c80afff5944b784d740201cc79a6f8
Oleksii Smirnov <o.smirnov@e-tender.ua>
2021-01-20 13:12:25 +0200

    add link to earth control-panel (to quick access)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// https://truthordare-database-default-rtdb.firebaseio.com/
 export const environment = {
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyC0ew6DkRueDMRPq13S6skPeXViN-unMC4",
     authDomain: "truthordare-database.firebaseapp.com",
     projectId: "truthordare-database",
-    storageBucket: "truthordare-database.appspot.com",
+    storageBucket: "truthordare-database-default-rtdb.firebaseio.com",
     messagingSenderId: "683895734418",
     appId: "1:683895734418:web:8f7961f2988f051f82e64b",
     measurementId: "G-0RNBMJ2EME"
