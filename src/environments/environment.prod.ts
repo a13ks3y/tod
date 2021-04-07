b4e71c7cfce81c12ae63c638a530a6b03cef3e3f
a13ks3y <a13ks3y.ass@gmail.com>
2021-04-08 02:40:46 +0300

    prepare to deploy to githubpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+    firebaseConfig: {
+    apiKey: "AIzaSyC0ew6DkRueDMRPq13S6skPeXViN-unMC4",
+    authDomain: "truthordare-database.firebaseapp.com",
+    projectId: "truthordare-database",
+    storageBucket: "truthordare-database.appspot.com",
+    messagingSenderId: "683895734418",
+    appId: "1:683895734418:web:8f7961f2988f051f82e64b",
+    measurementId: "G-0RNBMJ2EME"
+  }
 };
