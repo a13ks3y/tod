9e8ee8fa79922c5c85e1c695280bc45bee68af57
Oleksii Smirnov <a13ks3y@MacBook-Pro-citrus.local>
2021-04-08 01:42:14 +0300

    fix unit tests by cut off firebase

--- src/app/actions/actions.component.ts
+++ src/app/actions/actions.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
-import {from, Observable} from "rxjs";
 
 @Component({
   selector: 'tod-actions',
@@ -8,16 +6,10 @@ import {from, Observable} from "rxjs";
   styleUrls: ['./actions.component.less']
 })
 export class ActionsComponent implements OnInit {
-  action$: Observable<any>;
-  constructor(private fs: AngularFirestore) {
-    fs.firestore.settings({ experimentalForceLongPolling: true });
-    let actionsRef:AngularFirestoreCollection<string> = fs.collection('actions', ref => ref.limit(10));
-    this.action$ =  from(actionsRef.valueChanges()); //fs.collection('actions').valueChanges();
-    this.action$.subscribe();
-
-  }
+  constructor() {  }
 
   ngOnInit(): void {
+
 
   }
 
